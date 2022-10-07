2c21584c033d3069107c2b929c78f10f8da3eb4a
c1057@oic.jp <c1057@oic.jp>
2022-10-07 16:01:32 +0900

    a

--- my222022t/Form1.Designer.cs
+++ my222022t/Form1.Designer.cs
@@ -29,47 +29,26 @@ namespace my222022t
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(545, 47);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(646, 201);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 18);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-
         #endregion
+<<<<<<< HEAD
 =======
+>>>>>>> 0cdbbc85d5d6af212439c0f71af4c6144d11695a
+=======
+
+        private System.Windows.Forms.Button button1;
 >>>>>>> 0cdbbc85d5d6af212439c0f71af4c6144d11695a
     }
 }
